96f7ad494854c7ded02639dbfe3b2778d5710ad7
Yue Chen <chen-yue20@mails.tsinghua.edu.cn>
2024-05-17 11:02:49 +0000

    get logger and railroad from vscode plugin

--- src/main/java/org/cylindrical/iting/Main.java
+++ src/main/java/org/cylindrical/iting/Main.java
@@ -5,10 +5,15 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.cylindrical.iting.grammar.GQLLexer;
 import org.cylindrical.iting.grammar.GQLParser;
 
 public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         String expression = "MATCH (n:Person) WHERE n.name = 'Alice' RETURN n.age AS age, n.name AS name";
         CharStream input = CharStreams.fromString(expression);
@@ -22,5 +27,9 @@ public class Main {
         GQLParser parser = new GQLParser(tokens);
         ParseTree tree = parser.gql_program();
         System.out.println(tree.toStringTree(parser));
+
+        logger.info("This is an info message.");
+        logger.debug("This is a debug message.");
+        logger.error("This is an error message.");
     }
 }
